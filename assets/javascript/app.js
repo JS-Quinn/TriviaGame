57331a41a1b116abb2c79745d7f03fe55d3c206c
JS-Quinn <josephshannonquinn@Gmail.com>
2018-01-07 08:46:26 -0500

    final site edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,32 @@
 $(document).ready(function() {
 
-var number = 30; //declare timer value
 $("#start").on("click", start); // start a new game
 $("#submit").on("click", finish); // finish game
 $("#restart").on("click", restart); // restart game
+
 
 //  -------------------
 //  ESTABLISH FUNCTIONS
 //  -------------------
 
     function start() {
-        counter = setInterval(timer, 1000);
         showIt(".question");
         showIt("#submit");
         hideIt("#start");
         hideIt("#restart");
+        hideIt(".answers");
+        number = 31;
+        counter = setInterval(timer, 1000);
+        timer();
     }
     function timer() {
+        showIt("#show-timer");
         number-- // decrements timer by 1
         $("#show-timer").html("<h2>" + number + "</h2>");
         if (number === 0) {
-            alert("Time is up!")
-            stop(); //calls stop function
-        }
+           alert("Time is up! Check your answers.")
+           stop(); //calls stop function
+       }
     }
     function stop() {
         clearInterval(counter); //stopping the timer
@@ -30,9 +34,9 @@ $("#restart").on("click", restart); // restart game
         $(".answers").show();
         $(".question").hide();
         $("#submit").hide();
+        hideIt("#show-timer");
     }
     function finish () {
-        timer = 1;
         clearInterval(counter); //stops timer
         hideIt("#show-timer");
         hideIt("#submit");
@@ -40,7 +44,6 @@ $("#restart").on("click", restart); // restart game
         showIt(".answers");
         showIt("#restart");
         alert("Check your answers!");
-        timer();
     }
     function restart() {
         start();
@@ -52,5 +55,3 @@ $("#restart").on("click", restart); // restart game
         $(i).show();
     }
 });
-
-
